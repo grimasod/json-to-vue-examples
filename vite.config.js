194abec8f3c00a8b6443bc49cba3bc8bbb2a295a
David Grimason <>
2022-09-13 12:10:39 +0900

    Library build

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +12,27 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/main.js'),
+      name: 'JsonToVueExamples',
+      fileName: 'json-to-vue-examples'
+    },
+    rollupOptions: {
+      external: [
+        'vue',
+        'vue-router',
+        'json-to-vue'
+      ],
+      output: {
+        globals: {
+          vue: 'Vue',
+          'vue-router': 'VueRouter',
+          'json-to-vue': 'JsonToVue'
+        }
+      }
     }
   }
 })
