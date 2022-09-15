0860868e4b7dc3a13e9edb598ba4762507d62004
David Grimason <>
2022-09-15 09:43:22 +0900

    GTM

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import createRouter from './router/'
 import './main.css'
 import App from './App.vue'
+import VueGtag from 'vue-gtag'
 import MyGlobalFoo from '@/components/MyGlobalFoo.vue'
 import MyGlobalBar from '@/components/MyGlobalBar.vue'
 
@@ -10,3 +11,10 @@ const app = createApp(App)
 app.use(router)
 app.component('MyGlobalFoo', MyGlobalFoo).component('MyGlobalBar', MyGlobalBar)
 app.mount('#app')
+if (import.meta.env.VITE_GTM) {
+  app.use(VueGtag, {
+    config: {
+      id: import.meta.env.VITE_GTM,
+    },
+  }, router)
+}
