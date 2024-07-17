e39827bc1938d166b805e4653f118c883ebcee84
David Grimason <>
2024-07-17 13:27:25 +0900

    remove old site

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="sticky top-0 left-0 right-0 bg-gray flex flex-col items-center text-blue z-10 bg-opacity-95">
     <div class="w-full max-w-screen-md h-16 px-2 flex justify-between items-center relative">
-      <a href="https://circleoflifecycle.com/" class="flex items-center z-10 px-2">
+      <a href="https://example.com/" class="flex items-center z-10 px-2">
         <CircleLogoSpinner />
         <div class="flex items-baseline flex-col px-2 md:flex-row">
           <h1 class="text-2xl font-bold pl-2">
